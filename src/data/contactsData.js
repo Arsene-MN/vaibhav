8ae059559bdca50a1688dff60a9fe07c84d83eec
Arsene-MN <arsenemuvunyi27@gmail.com>
2023-09-05 21:24:04 +0200

    Remove 'node_modules' from Git history

---
+++ src/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: 'arsenemuvunyi27@gmail.com',
+    phone: '+8801608797655',
+    address: 'India ',
+    github: 'https://github.com/',
+    facebook: 'https://www.facebook.com/',
+    linkedIn: 'https://www.linkedin.com//',
+    twitter: 'https://twitter.com/',
+    medium: 'https://medium.com/',
+    stackOverflow: 'https://stackoverflow.com/',
+    devUsername: "vaibhav"
+}
